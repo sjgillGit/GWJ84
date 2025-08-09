f0c188012d81c1c284453caa68a2f33d567ae332
MeGl <54992889+EndGameGl@users.noreply.github.com>
2025-08-09 20:36:26 +0300

    Added gravity to player controller

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -16,19 +16,23 @@ var camera_controller: CameraController;
 @export
 var health_handler: EntityHealthHandler;
 
+var gravity: float;
+
+func _ready() -> void:
+	gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 func _physics_process(_delta: float) -> void:
 	var input: Vector2 = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBack");
-	if input == Vector2.ZERO:
-		return;
-
 	var movement_direction : Vector3 = Vector3(input.x, 0.0, input.y).rotated(Vector3.UP, camera_controller.get_horizontal_rotation());
-	mesh_root.look_at(global_position + Vector3(movement_direction.x, 0, movement_direction.z));
+	
+	if input != Vector2.ZERO:
+		mesh_root.look_at(global_position + Vector3(movement_direction.x, 0, movement_direction.z));
 
 	var movement_velocity = movement_direction * move_speed;
 	if Input.is_action_pressed("Sprint"):
 		movement_velocity *= sprint_multiplier;
 
-	self.velocity = Vector3(movement_velocity.x, -0.98, movement_velocity.z);
+	self.velocity = Vector3(movement_velocity.x, -gravity, movement_velocity.z);
 	self.move_and_slide();
 
 func take_damage(damage: float) -> void:
