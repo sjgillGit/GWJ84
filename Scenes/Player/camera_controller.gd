018d819c0d023879dc7604f43d09b8c30a902272
MeGl <54992889+EndGameGl@users.noreply.github.com>
2025-08-09 17:13:45 +0300

    Some quick refactoring and property grouping

--- Scenes/Player/camera_controller.gd
+++ Scenes/Player/camera_controller.gd
@@ -1,11 +1,25 @@
 extends Node3D
 class_name CameraController
 
+@export_group("Horizontal settings")
 @export
 var camera_horizontal: Node3D;
 
 @export
+var horizontal_sensitivity: float = 0.07;
+
+@export
+var horizontal_acceleration: float = 15;
+
+@export_group("Vertical settings")
+@export
 var camera_vertical: Node3D;
+
+@export
+var vertical_sensitivity: float = 0.07;
+
+@export
+var vertical_acceleration: float = 15;
 
 @export
 var vertical_min: float = -55;
@@ -13,19 +27,15 @@ var vertical_min: float = -55;
 @export
 var vertical_max: float = 0;
 
-var camera: Camera3D;
-
 var horizontal_movement: float = 0.0;
 var vertical_movement: float = 0.0;
 
-var horizontal_sensitivity: float = 0.07;
-var vertical_sensitivity: float = 0.07;
-
-var horizontal_acceleration: float = 15;
-var vertical_acceleration: float = 15;
+## default camera offset that was saved during scene startup
+var default_camera_offset: Vector3;
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+	default_camera_offset = position;
 
 func _input(event: InputEvent) -> void:
 	if event is not InputEventMouseMotion:
@@ -39,3 +49,6 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	camera_horizontal.rotation_degrees.y = lerp(camera_horizontal.rotation_degrees.y, horizontal_movement, horizontal_acceleration * delta);
 	camera_vertical.rotation_degrees.x = lerp(camera_vertical.rotation_degrees.x, vertical_movement, vertical_acceleration * delta);
+
+func get_horizontal_rotation() -> float:
+	return camera_horizontal.rotation.y
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,25 +1,29 @@
 extends CharacterBody3D
 class_name Player
 
+## Emitted when player loses all Health points
+## [br][param player] Player node reference
+signal player_died(player: Player);
+
 @export
 var move_speed: float;
 
 @export
 var sprint_multiplier: float;
 
-@export 
-var camera: Camera3D;
+@export
+var mesh_root: Node3D;
 
 @export
-var modelRoot: Node3D;
+var camera_controller: CameraController;
 
 func _physics_process(_delta: float) -> void:
 	var input: Vector2 = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBack");
 	if input == Vector2.ZERO:
 		return;
 
-	var movement_direction : Vector3 = Vector3(input.x, 0.0, input.y).rotated(Vector3.UP, $CameraRoot/CameraHorizontal.rotation.y);
-	modelRoot.look_at(global_position + Vector3(movement_direction.x, 0, movement_direction.z));
+	var movement_direction : Vector3 = Vector3(input.x, 0.0, input.y).rotated(Vector3.UP, camera_controller.get_horizontal_rotation());
+	mesh_root.look_at(global_position + Vector3(movement_direction.x, 0, movement_direction.z));
 
 	var movement_velocity = movement_direction * move_speed;
 	if Input.is_action_pressed("Sprint"):
@@ -27,3 +31,6 @@ func _physics_process(_delta: float) -> void:
 
 	self.velocity = Vector3(movement_velocity.x, -0.98, movement_velocity.z);
 	self.move_and_slide();
+
+func take_damage(damage: float) -> void:
+	player_died.emit(self);
