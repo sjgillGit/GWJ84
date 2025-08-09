352b278421bfc0e519286dd5c3cb2d146dcb2790
MeGl <54992889+EndGameGl@users.noreply.github.com>
2025-08-09 18:19:12 +0300

    Added action system with ability to choose actions and perform them

---
+++ Scenes/Player/Actions/player_melee_action.gd
@@ -0,0 +1,26 @@
+extends PlayerActionBase
+class_name PlayerMeleeAction
+
+@export
+var melee_cooldown: float;
+
+var current_cooldown: float;
+var can_be_performed: bool = true;
+
+func _ready() -> void:
+	set_process(false);
+
+func _process(delta: float) -> void:
+	current_cooldown -= delta;
+	if current_cooldown <= 0.0:
+		can_be_performed = true;
+		set_process(false);
+
+func _can_perform_action() -> bool:
+	return can_be_performed;
+
+func _perform_action() -> void:
+	current_cooldown = melee_cooldown;
+	can_be_performed = false;
+	print("Melee action performed");
+	set_process(true);
