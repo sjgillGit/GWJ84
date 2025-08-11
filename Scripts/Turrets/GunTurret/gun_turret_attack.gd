61495e9a34813e61dbe14a7c0fb913ac8db25811
MeGl <54992889+EndGameGl@users.noreply.github.com>
2025-08-11 12:06:44 +0300

    Saving turret bone rotation

--- Scripts/Turrets/GunTurret/gun_turret_attack.gd
+++ Scripts/Turrets/GunTurret/gun_turret_attack.gd
@@ -12,21 +12,26 @@ var skeleton: Skeleton3D;
 
 var target: Node3D;
 var bone_idx: int;
+var bone_default_position: Vector3;
 
 static func _get_state_name() -> String:
 	return "gun_turret_attack";
 
 func _ready() -> void:
-	bone_idx = skeleton.find_bone("Bone.019");
+	bone_idx = skeleton.find_bone("central_bone");
+	bone_default_position = skeleton.get_bone_pose_position(bone_idx);
 
 func _on_enter() -> void:
 	entity_detector.entity_lost.connect(_on_entity_lost);
 	animation_player.stop();
+	bone_default_position = skeleton.get_bone_pose_position(bone_idx);
 
 func _on_process(_delta: float):
 	var bone_transform: Transform3D = skeleton.global_transform * skeleton.get_bone_global_pose(bone_idx);
-	bone_transform = bone_transform.looking_at(target.global_position, Vector3(-1, 1, -1), true);
-	skeleton.set_bone_global_pose_override(bone_idx, skeleton.global_transform.affine_inverse() * bone_transform, 1.0, true);
+	bone_transform = bone_transform.looking_at(target.global_position, Vector3.UP, true);
+	var local_bone_transform: Transform3D = skeleton.global_transform.affine_inverse() * bone_transform;
+	var bone_rotation: Quaternion = local_bone_transform.basis.get_rotation_quaternion();
+	skeleton.set_bone_pose_rotation(bone_idx, bone_rotation);
 
 func _on_exit() -> void:
 	entity_detector.entity_lost.disconnect(_on_entity_lost);
