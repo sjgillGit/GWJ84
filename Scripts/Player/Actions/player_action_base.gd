571149b3fd715f24c1da1d50d807ce03ece0b8f7
Tungill <software@tungill.com>
2025-08-11 15:36:39 +0200

    Renaming animations ressource + remove loop
    # Added
    - new signal in player_action_base.gd to emit an animation to play
    - set up animation list in Player.tscn

--- Scripts/Player/Actions/player_action_base.gd
+++ Scripts/Player/Actions/player_action_base.gd
@@ -1,5 +1,13 @@
 extends Node3D
 class_name PlayerActionBase
+## To emit when a new animation should be triggered. [br]
+## Tell to the listener:[br][param new_state]: action single keyword -> for humans to understand the origin,[br]
+## [param new_anims]: the list of animations -> in case one action can trigger various animations contextually,[br]
+## [param anim_index]: the index of the animation to use from [param new_anims].
+signal state_changed(new_state: String, new_anims: Array[Animation], anim_index: int)
+## The list of animations that this component can trigger.[br]Used in [signal PlayerActionBase.state_changed].
+@export
+var animations: Array[Animation]
 
 func _can_perform_action() -> bool:
 	return false;
