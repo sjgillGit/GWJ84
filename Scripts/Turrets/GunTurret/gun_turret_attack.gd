64b1e660855176107dd677db22e618baa30f3734
MeGl <54992889+EndGameGl@users.noreply.github.com>
2025-08-11 12:53:18 +0300

    Added logic when turret is supposed to shoot

--- Scripts/Turrets/GunTurret/gun_turret_attack.gd
+++ Scripts/Turrets/GunTurret/gun_turret_attack.gd
@@ -10,8 +10,12 @@ var animation_player: AnimationPlayer;
 @export
 var skeleton: Skeleton3D;
 
+@export
+var shoot_interval: float;
+
 var target: Node3D;
 var bone_idx: int;
+var shoot_interval_timer: float;
 
 static func _get_state_name() -> String:
 	return "gun_turret_attack";
@@ -22,13 +26,11 @@ func _ready() -> void:
 func _on_enter() -> void:
 	entity_detector.entity_lost.connect(_on_entity_lost);
 	animation_player.stop();
+	shoot_interval_timer = 0.0;
 
-func _on_process(_delta: float):
-	var bone_transform: Transform3D = skeleton.global_transform * skeleton.get_bone_global_pose(bone_idx);
-	bone_transform = bone_transform.looking_at(target.global_position, Vector3.UP, true);
-	var local_bone_transform: Transform3D = skeleton.global_transform.affine_inverse() * bone_transform;
-	var bone_rotation: Quaternion = local_bone_transform.basis.get_rotation_quaternion();
-	skeleton.set_bone_pose_rotation(bone_idx, bone_rotation);
+func _on_process(delta: float) -> void:
+	look_at_target();
+	shoot_at_target(delta);
 
 func _on_exit() -> void:
 	entity_detector.entity_lost.disconnect(_on_entity_lost);
@@ -38,3 +40,16 @@ func _on_entity_lost(node: Node3D) -> void:
 	if node != target:
 		return;
 	transition_to_state(GunTurretIdle._get_state_name());
+
+func look_at_target() -> void:
+	var bone_transform: Transform3D = skeleton.global_transform * skeleton.get_bone_global_pose(bone_idx);
+	bone_transform = bone_transform.looking_at(target.global_position, Vector3.UP, true);
+	var local_bone_transform: Transform3D = skeleton.global_transform.affine_inverse() * bone_transform;
+	var bone_rotation: Quaternion = local_bone_transform.basis.get_rotation_quaternion();
+	skeleton.set_bone_pose_rotation(bone_idx, bone_rotation);
+
+func shoot_at_target(delta: float) -> void:
+	shoot_interval_timer += delta;
+	if shoot_interval_timer >= shoot_interval:
+		print("turret shoots at the target");
+		shoot_interval_timer = 0.0;
