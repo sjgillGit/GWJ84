e3f3e4431759d80c4e84ef807fca8df38e5ff760
Ian <ian@TOMATO.PC>
2025-08-14 22:33:39 -0300

    refactored rat class + created NPC class

--- Scripts/NPCs/npc.gd
+++ Scripts/NPCs/npc.gd
@@ -1,5 +1,5 @@
 extends CharacterBody3D
-class_name Rat
+class_name Npc
 @export var speed = 10
 @export var acceleration = 10 
 @export var attack_radius = 5
@@ -9,11 +9,8 @@ signal chasing
 signal attacking
 @export var end_patrol_position: Marker3D
 @export var attack_damage:int = 10
-@onready  var raycasts = [
-$Rat/raycasts/forward_raycast,
-$Rat/raycasts/positive_45_raycast,
-$Rat/raycasts/minus_45_raycast
-]
+var raycasts 
+var animation_player
 var begin_patrol_position: Vector3
 var current_target: Vector3
 var state:RatState
@@ -29,34 +26,6 @@ func _ready():
 func update_neighbors(new_neighbors):
 	neighbors = new_neighbors
 
-func flip_patrol_target():
-	if current_target==begin_patrol_position:
-		current_target = end_patrol_position.global_position
-	else:
-		current_target = begin_patrol_position
-
-func patrol_behaviour(delta):
-	patrolling.emit()
-	boid_calculation((current_target - global_position).normalized() * speed,delta)
-	if abs(current_target.x + current_target.z - global_position.x - global_position.z) < 2:
-		flip_patrol_target()
-
-func chase_behaviour(delta):
-	velocity = Vector3.ZERO
-	chasing.emit()
-	current_target = player.global_position
-	boid_calculation((player.global_position - global_position).normalized()*speed,delta)
-	if (player.global_position - global_position).length() < attack_radius:
-		state = AttackRatState.new(self)
-
-func attack_behaviour(delta):
-	if can_attack:
-		velocity = Vector3.ZERO
-		$attack_cooldown_timer.start()
-		attacking.emit()
-		player.take_damage(attack_damage)
-		can_attack = false
-
 func get_alignment_force():
 	var avg_velocity = Vector3.ZERO
 	var count =0
@@ -67,7 +36,6 @@ func get_alignment_force():
 		avg_velocity /= count
 		avg_velocity =  (avg_velocity.normalized()*speed) - velocity
 	return avg_velocity
-
 
 func get_avoidance_force():
 	var avoidance_force = Vector3.ZERO
@@ -119,12 +87,6 @@ func alert_neighbors(body_rid, body, body_shape_index, local_shape_index):
 	for neighbor in neighbors:
 		neighbor.try_chase_player(body_rid, body, body_shape_index, local_shape_index)
 
-func chase_player(body_rid, body, body_shape_index, local_shape_index):
-	state = ChaseRatState.new(self)
-	current_target = body.global_position
-	player = body
-	velocity = Vector3.ZERO
-
 func start_in_leader_status():
 	state = PatrolRatState.new(self)
 
@@ -141,17 +103,13 @@ func take_damage(damage):
 func get_stunned():
 	state = StunnedRatState.new(self,state)
 	$stunned_cooldown.start()
-	$Rat/AnimationPlayer.play("Stunned")
+	animation_player.play("Stunned")
 
 func _on_death():
 	velocity = Vector3.ZERO
 
-
-
-
 func _on_animation_player_death_animation_finished():
 	killed.emit(self)
-
 
 func _on_stunned_cooldown_timeout():
 	state=state.get_previous_state()
